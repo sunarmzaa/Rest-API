b15c4af49fc07be61c45df859b52674ba55fe871
sunarmzaa <sunarmzaa@gmail.com>
2021-03-16 00:43:08 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,78 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+const URL = 'http://localhost/api/bears'
+//npm install -g create-next-app
+//create-next-app hello-world
+//npm i -s axios
+
+
+
 
 export default function Home() {
+  const [bears, setBears] = useState({
+    list: [
+      { id: 1, name: 'winnie', weight: 22 }
+    ]
+  })
+
+  const [name, setName] = useState('')
+  const [weight, setWeight] = useState(0)
+  const [bear, setBear] = useState({}) 
+
+
+  useEffect(() => { getBears() }, [])
+
+  const getBears = async () => {
+    let bears = await axios.get(URL)
+    setBears(bears.data)
+    console.log('Bears: ', bears.data['list'])
+
+  }
+
+  const printBears = () => {
+    if (bears.list && bears.list.length)
+      return bears.list.map(
+        (item, index) =>
+          <li key={index}>{item.id} :
+            {item.name} :
+            {item.weight}
+            <button onClick={() => getBear(item.id)}>Get</button>
+            <button onClick={() => updateBear(item.id)}>Update</button>
+          </li>)
+    else {
+      return (<li>No Bear</li>)
+    }
+  }
+
+  const getBear = async (id) => {
+    let bear = await axios.get(`${URL}/${id}`)
+      setBear({ name: bear.data.name, weight: bear.data.weight })
+  }
+
+  const addBear = async (name, weight) => {
+    let bears = await axios.post(URL, { name, weight })
+    setBears(bears.data)
+  }
+
+
+  const updateBear = async(id) => {
+    let bears = await axios.put(`${URL}/${id}`, {name,weight})
+    setBears(bears.data)
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>Bears
+      <ul>
+        {printBears()}
+      </ul>
+      <div>
+        Selected: {bear.name} {bear.weight}
+      </div>
+      <h2>Add bear</h2>
+      Name: <input type="text" onChange={(e) => setName(e.target.value)} /> <br />
+      Weight: <input type="number" onChange={(e) => setWeight(e.target.value)} /> <br />
+      <button onClick={() => addBear(name, weight)}>Add</button>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
